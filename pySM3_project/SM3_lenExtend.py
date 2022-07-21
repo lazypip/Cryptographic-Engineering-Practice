0e4278554bec969f8a5b73f9635bc9dd16b37d40
wyb <wuyanbo@mail.sdu.edu.cn>
2022-07-21 17:33:32 +0800

    SM3 长度扩展

---
+++ pySM3_project/SM3_lenExtend.py
@@ -0,0 +1,23 @@
+"""TODO : SM3长度扩展攻击"""
+from SM3 import SM3
+from typing import *
+
+
+def SM3_extend(extend_msg: str, prehash: List[int]) -> None:
+    extend_msg_len = len(extend_msg)
+    padding_1 = b''
+    extend_msg = extend_msg
+    padding_2 = b''
+    whole_msg = b''  # padding_1 || msg || padding_2
+
+    """ [输出hash(M || padding_1 || extend_msg || padding_2)] """
+    hash = SM3(extend_msg, IV = prehash)
+    print(hash.getResult())
+
+
+if __name__ == "__main__":
+    prehash: SM3 = SM3("abcd")
+    prehash = prehash.getResult(hexdig=False)
+
+    extend_msg = "efgh"
+    SM3_extend(extend_msg, prehash)
